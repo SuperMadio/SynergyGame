39f8ae6d3050386d2c167214f7a15661c165d0b2
DLuna <a.ssabp@gmail.com>
2017-09-26 23:44:11 +0300

    commit from correct folder, with updated Player.cs script

---
+++ SynergyMaze/Assets/C# Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+    private Transform heroObj;
+	private Animator myAnimator; // use this to change the animation
+    private Animation myAnimation;
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator> ();
+        myAnimation = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxis("Horizontal"); //x axis
+        float ver = Input.GetAxis("Vertical"); //y axis
+
+        // TODO
+        // Change the animiation of the player object to match
+        // the direction is moving towards.
+
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            myAnimator.SetBool ("left", true);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+            myAnimator.SetBool ("right", true);
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			myAnimator.SetBool ("up", true);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			myAnimator.SetBool ("down", true);
+		} else {
+			myAnimator.SetBool ("down", false);
+			myAnimator.SetBool ("up", false);
+			myAnimator.SetBool ("right", false);
+			myAnimator.SetBool ("left", false);
+
+            myAnimator.SetBool ("idle", true);
+
+            myRigidbody.velocity = new Vector2(0,0);
+		}
+
+        HandleMovement(hor, ver);
+    }
+
+    private void HandleMovement(float horizontal, float vertical) {
+        // myRigidbody.velocity = Vector2.left;
+        myRigidbody.velocity = new Vector2(vertical, myRigidbody.velocity.y);
+        myRigidbody.velocity = new Vector2(horizontal , myRigidbody.velocity.x);
+    }
+}
