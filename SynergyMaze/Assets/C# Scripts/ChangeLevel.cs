12c2c152b7fe8f889914048df14ff79cfdbf5fdd
DLuna <a.ssabp@gmail.com>
2017-10-09 23:42:40 +0300

    Fixed the bug that caused transitions between level.

---
+++ SynergyMaze/Assets/C# Scripts/ChangeLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeLevel : MonoBehaviour {
+	
+	void OnTriggerEnter2D (Collider2D other){
+        Debug.Log("im at least inside");
+        if(other.gameObject.name == "Player")
+        {
+            Debug.Log("its colliding! ");
+            SceneManager.LoadScene("Level_Two");
+        }
+
+    }
+}
--- SynergyMaze/Assets/C# Scripts/Player.cs
+++ SynergyMaze/Assets/C# Scripts/Player.cs
@@ -7,12 +7,12 @@ public class Player : MonoBehaviour {
     private Rigidbody2D myRigidbody;
     private Transform heroObj;
 	private Animator myAnimator; // use this to change the animation
-    private Animation myAnimation;
+    //private Animation myAnimation;
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
 		myAnimator = GetComponent<Animator> ();
-        myAnimation = GetComponent<Animation>();
+        //myAnimation = GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
@@ -24,25 +24,24 @@ public class Player : MonoBehaviour {
         // Change the animiation of the player object to match
         // the direction is moving towards.
 
-        if (Input.GetKey (KeyCode.LeftArrow)) {
+        if (myRigidbody.velocity.x < 0) { //Input.GetKey (KeyCode.LeftArrow)
             myAnimator.SetBool ("left", true);
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
+		} else if (myRigidbody.velocity.x > 0) { //Input.GetKey (KeyCode.RightArrow)
             myAnimator.SetBool ("right", true);
-		} else if (Input.GetKey (KeyCode.UpArrow)) {
+        } else if (myRigidbody.velocity.y > 0) { //Input.GetKey (KeyCode.UpArrow)
 			myAnimator.SetBool ("up", true);
-		} else if (Input.GetKey (KeyCode.DownArrow)) {
+        } else if (myRigidbody.velocity.y < 0) { //Input.GetKey (KeyCode.DownArrow)
 			myAnimator.SetBool ("down", true);
-		} else {
-			myAnimator.SetBool ("down", false);
+        } else {
+            myAnimator.SetBool("idle", true);
+            myAnimator.SetBool ("down", false);
 			myAnimator.SetBool ("up", false);
 			myAnimator.SetBool ("right", false);
-			myAnimator.SetBool ("left", false);
+            myAnimator.SetBool("left", false);
 
-            myAnimator.SetBool ("idle", true);
-
-            myRigidbody.velocity = new Vector2(0,0);
-		}
-
+            myRigidbody.velocity = new Vector2(0, 0);
+        }
+        
         HandleMovement(hor, ver);
     }
 
