886453c32ade3bbfe3a3bcfbd8040414a24d7c24
DLuna <a.ssabp@gmail.com>
2017-10-16 21:25:24 +0300

    -- Moonwalk bug fixed -- Fade in effect added -- New script (GameManager) added

---
+++ SynergyMaze/Assets/C# Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public Image image;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Destroy(image,1);
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Winning Screen");
+        }
+    }
+
+    public void changeToScene(int toScene)
+    {
+
+        SceneManager.LoadScene(toScene);
+    }
+}
--- SynergyMaze/Assets/C# Scripts/Player.cs
+++ SynergyMaze/Assets/C# Scripts/Player.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    
     private Rigidbody2D myRigidbody;
-    private Transform heroObj;
-	private Animator myAnimator; // use this to change the animation
-    //private Animation myAnimation;
-	// Use this for initialization
-	void Start () {
+	private Animator myAnimator; // use to change the animation
+
+    [SerializeField]
+    private float movementSpeed; // use to control player movement speed
+
+    // Use this for initialization
+    void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
 		myAnimator = GetComponent<Animator> ();
-        //myAnimation = GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
@@ -24,30 +26,26 @@ public class Player : MonoBehaviour {
         // Change the animiation of the player object to match
         // the direction is moving towards.
 
-        if (myRigidbody.velocity.x < 0) { //Input.GetKey (KeyCode.LeftArrow)
-            myAnimator.SetBool ("left", true);
-		} else if (myRigidbody.velocity.x > 0) { //Input.GetKey (KeyCode.RightArrow)
-            myAnimator.SetBool ("right", true);
-        } else if (myRigidbody.velocity.y > 0) { //Input.GetKey (KeyCode.UpArrow)
-			myAnimator.SetBool ("up", true);
-        } else if (myRigidbody.velocity.y < 0) { //Input.GetKey (KeyCode.DownArrow)
-			myAnimator.SetBool ("down", true);
-        } else {
-            myAnimator.SetBool("idle", true);
-            myAnimator.SetBool ("down", false);
-			myAnimator.SetBool ("up", false);
-			myAnimator.SetBool ("right", false);
-            myAnimator.SetBool("left", false);
-
-            myRigidbody.velocity = new Vector2(0, 0);
+        if (myRigidbody.velocity.x < -0.01 && myRigidbody.velocity.y == 0) {
+            myAnimator.SetInteger("state", 4);
+		}
+        else if (myRigidbody.velocity.x > 0.01 && myRigidbody.velocity.y == 0) {
+            myAnimator.SetInteger("state", 2);
+        }
+        else if (myRigidbody.velocity.y > 0.01 && myRigidbody.velocity.x == 0) {
+            myAnimator.SetInteger("state", 1);
+        }
+        else if (myRigidbody.velocity.y < -0.01 && myRigidbody.velocity.x == 0) {
+            myAnimator.SetInteger("state", 3);
+        }
+        else {
+            myAnimator.SetInteger("state", 0);
         }
         
         HandleMovement(hor, ver);
     }
 
     private void HandleMovement(float horizontal, float vertical) {
-        // myRigidbody.velocity = Vector2.left;
-        myRigidbody.velocity = new Vector2(vertical, myRigidbody.velocity.y);
-        myRigidbody.velocity = new Vector2(horizontal , myRigidbody.velocity.x);
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
     }
 }
